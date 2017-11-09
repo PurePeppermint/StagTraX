4fe484fa731ec62d7952507e3ef064aafb8edaee
William Milkovic <wmilkovic@rocketfuel.com>
2017-11-08 22:06:46 -0500

    Initial commit

---
+++ app/src/main/java/edu/fairfield/stagtrax/LoginActivity.java
@@ -0,0 +1,44 @@
+package edu.fairfield.stagtrax;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.content.Intent;
+import org.apache.commons.validator.routines.EmailValidator;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final Button button = findViewById(R.id.login);
+        final EditText email = findViewById(R.id.email);
+        final EditText password = findViewById(R.id.password);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (email.getText().toString().trim().isEmpty()) {
+                    email.setError("You must enter an email!");
+                } else if (!EmailValidator.getInstance().isValid(email.getText().toString())) {
+                    email.setError("Email is not valid!");
+                } else {
+                    if (password.getText().toString().isEmpty()) {
+                        password.setError("Password is empty!");
+                        //Show register user activity
+
+                        Intent intent = new Intent(this, RegisterActivity.class);
+                        intent.putExtra("Email", email.getText().toString());
+                        startActivity(intent);
+
+                    } else {
+                        password.setError("Password is not empty");
+                        //Validate user then show main activity
+                    }
+                }
+            }
+        });
+    }
+}

9c38e6232dff7c9f79e24ff3657e18d063140aef
William Milkovic <wmilkovic@rocketfuel.com>
2017-11-13 15:48:56 -0500

    Updated bottom navigation. Added password reset button.

--- app/src/main/java/edu/fairfield/stagtrax/LoginActivity.java
+++ app/src/main/java/edu/fairfield/stagtrax/LoginActivity.java
@@ -6,39 +6,62 @@ import android.widget.Button;
 import android.view.View;
 import android.widget.EditText;
 import android.content.Intent;
-//import android.widget.Toast;
 import org.apache.commons.validator.routines.EmailValidator;
 
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final Button button = findViewById(R.id.login);
-        final EditText email = findViewById(R.id.email);
-        final EditText password = findViewById(R.id.password);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button loginButton = findViewById(R.id.login);
+        final Button resetButton = findViewById(R.id.reset);
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if (email.getText().toString().trim().isEmpty()) {
-                    email.setError("You must enter an email!");
-                } else if (!EmailValidator.getInstance().isValid(email.getText().toString())) {
-                    email.setError("Email is not valid!");
-                } else {
-                    email.setError(null);
-                    if (password.getText().toString().isEmpty()) {
-                        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                        intent.putExtra("email", email.getText().toString());
-                        startActivity(intent);
-                    } else {
-                        //Toast.makeText(getBaseContext(), "Login is not implemented!", Toast.LENGTH_LONG).show();
-                        //TODO Validate user
-                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                        startActivity(intent);
-                    }
-                }
+                restPassword();
             }
         });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                loginOrRegister();
+            }
+        });
+    }
+
+    private void loginOrRegister() {
+        final EditText email = findViewById(R.id.email);
+        final EditText password = findViewById(R.id.password);
+
+        if (email.getText().toString().trim().isEmpty()) {
+            email.setError("You must enter an email!");
+        } else if (!EmailValidator.getInstance().isValid(email.getText().toString())) {
+            email.setError("Email is not valid!");
+        } else {
+            email.setError(null);
+            if (password.getText().toString().isEmpty()) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                intent.putExtra("email", email.getText().toString());
+                startActivity(intent);
+            } else {
+                //TODO Validate user
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        }
+    }
+
+    private void restPassword() {
+        final EditText email = findViewById(R.id.email);
+
+        if (email.getText().toString().trim().isEmpty()) {
+            email.setError("You must enter an email!");
+        } else if (!EmailValidator.getInstance().isValid(email.getText().toString())) {
+            email.setError("Email is not valid!");
+        } else {
+            email.setError(null);
+            //TODO Reset user's password
+        }
     }
 }
